b50ffc422d31f809cc01b344a6b26f9777b11942
Yiğit Emin KOLAN <74785479+NexDen@users.noreply.github.com>
2024-01-01 16:22:00 +0300

    Add files via upload

---
+++ Assets/UIHandler.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Random = System.Random;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIHandler : MonoBehaviour
+{
+    [SerializeField] List<GameObject> toBeRemoved;
+    [SerializeField] public Text PbText;
+    public Text clock;
+
+    public void HideScreen(){
+        foreach(GameObject g in toBeRemoved){
+            g.gameObject.SetActive(false);
+        }
+    }
+
+    public void ShowScreen(){
+        foreach(GameObject g in toBeRemoved){
+            g.gameObject.SetActive(true);
+        }
+    }
+    
+    void Update(){
+        clock.text = System.DateTime.Now.ToString("HH:mm");
+    }
+
+}
